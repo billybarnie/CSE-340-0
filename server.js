285d700859ed925d30bf9e99a23f4b975435857c
billybarnie <haefguafe@gmail.com>
2024-10-05 19:09:44 -0500

    updating error for task 3

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(static)
   app.use("/inv", inventoryRoute)
   // File Not Found Route - must be last route in list
   app.use(async (req, res, next) => {
-    next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+    next({status: 404, message: 'Sorry, we appear to have lost that page. Your eyes betray you. It’s not here … Really! It’s all in your mind.'})
   })
 
 /* ***********************
@@ -42,7 +42,7 @@ app.use(static)
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
-  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route? Your eyes betray you. It’s not here … Really! It’s all in your mind.'}
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route? '}
   res.render("errors/error", {
     title: err.status || 'Server Error',
     message,
